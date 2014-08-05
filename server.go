d56250b6fd05be7195d9828f1618105784d9637b
wmak <william.mak@hotmail.ca>
2014-08-05 16:00:15 +0000

    Adding analysis to the request

--- server.go
+++ server.go
@@ -5,15 +5,28 @@ import (
 	"crypto/md5"
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os/exec"
 )
 
 type ImageUpload struct {
 	User  string
 	Image string
+}
+
+type ImageResult struct {
+	Faces string
+}
+
+func analysis(ws *websocket.Conn, path string) {
+	out, err := exec.Command("python2.7", "analysis/analysis.py", path).Output()
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+	ws.Write(out)
+	log.Printf("Analysis complete on %s", path)
 }
 
 func imageHandler(ws *websocket.Conn) {
@@ -22,20 +35,22 @@ func imageHandler(ws *websocket.Conn) {
 		log.Printf("Error in the image handler %s", err)
 	}
 	//confirm data.User
+	log.Printf("Connection from %s", data.User)
 	Image, err := base64.StdEncoding.DecodeString(data.Image)
 	if err != nil {
 		log.Fatal("error:", err)
 	}
-
 	hasher := md5.New()
 	hasher.Write([]byte(Image))
 	Sum := hex.EncodeToString(hasher.Sum(nil))
-	ioutil.WriteFile("images/"+Sum+".jpg", Image, 0644)
-	log.Printf("Saved new image at %s", "images/"+Sum+".jpg")
+	path := "images/" + Sum + ".jpg"
+	ioutil.WriteFile(path, Image, 0644)
+	log.Printf("Saved new image at %s", path)
+	analysis(ws, path)
 }
 
 func main() {
-	fmt.Println("Starting Lyra Server")
+	log.Println("Starting Lyra Server")
 	http.Handle("/image", websocket.Handler(imageHandler))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
