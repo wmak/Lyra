5d1d1e34b482d402ef6c234e788506e15b1e42c2
wmak <william.mak@hotmail.ca>
2014-08-06 16:50:10 +0000

    fixing some formatting, and adding debug

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -5,6 +5,7 @@ import cv2
 import cv2.cv as cv
 import sys
 import time
+import colorsys
 
 if __name__ == "__main__":
     # Debug
@@ -15,25 +16,33 @@ if __name__ == "__main__":
     filename = sys.argv[1]
     result = {}
     image = cv2.imread(filename)
-    gray_image = cv2.cvtColor(image, cv.CV_RGB2GRAY)
-    gray_image = cv2.equalizeHist(gray_image)
+    gray_image = cv2.equalizeHist(cv2.cvtColor(image, cv.CV_RGB2GRAY))
 
     #Face Detection
     face_cascade = cv2.CascadeClassifier('analysis/haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(gray_image, 1.1, 2, cv.CV_HAAR_SCALE_IMAGE,
-            (20,20))
+    faces = face_cascade.detectMultiScale(gray_image, 1.1, 2, cv.CV_HAAR_SCALE_IMAGE, (20,20))
     result.setdefault("Faces", len(faces))
 
     #Colour Analysis
     Z = np.float32(image.reshape((-1, 3)))
-    criteria = (cv2.TERM_CRITERIA_EPS, 7, 1.0)
-    _, label, center = cv2.kmeans(Z,2,criteria,10,cv2.KMEANS_PP_CENTERS)
-    result.setdefault("Primary", [center[0][2], center[0][1], center[0][0]])
-    result.setdefault("Secondary", [center[1][2], center[1][1], center[1][0]])
+    criteria = (cv2.TERM_CRITERIA_EPS, 6, 1.0)
+    _, label, center = cv2.kmeans(Z,2,criteria, 7,cv2.KMEANS_PP_CENTERS)
+    #Correcting for BGR ordering ):
+    primary_rgb = list(center[0])
+    secondary_rgb = list(center[1])
+    primary_rgb.reverse()
+    secondary_rgb.reverse()
+    #Calculating HSV
+    (primary_hue, _, _) = colorsys.rgb_to_hls(primary_rgb[0]/256.0, primary_rgb[1]/256.0, primary_rgb[2]/256.0)
+    (secondary_hue, _, _) = colorsys.rgb_to_hls(secondary_rgb[0]/256.0, secondary_rgb[1]/256.0, secondary_rgb[2]/256.0)
+
+    result.setdefault("Primary", {"rgb" : primary_rgb, "hue" : primary_hue*360})
+    result.setdefault("Secondary", {"rgb" : secondary_rgb, "hue" : secondary_hue*360})
 
     # Debug
     if debug:
-        print("--- %f seconds ---" % (time.time() - start_time))
+        # Average run time: 2.8s
+        print("--- %f seconds ---" % ((time.time() - start_time)/1.0))
         center = np.uint8(center)
         res = center[label.flatten()]
         res2 = res.reshape((image.shape))
