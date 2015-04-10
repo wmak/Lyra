0a0321e592847a04c500be1a583d8c78760d3293
William Mak <william.mak@hotmail.ca>
2015-04-10 13:45:09 -0400

    Added loudness as a feature

--- analysis/music/feature_extractor.py
+++ analysis/music/feature_extractor.py
@@ -11,8 +11,9 @@ def analyze_file(filename):
     engine = Engine()
     file_processor = AudioFileProcessor()
 
-    fp.addFeature('mfcc: MFCC blockSize=256 stepSize=128')
-    fp.addFeature('ComplexDomainOnsetDetection: ComplexDomainOnsetDetection blockSize=512 stepSize=256')
+    fp.addFeature('mfcc: MFCC blockSize=512 stepSize=256')
+    #fp.addFeature('ComplexDomainOnsetDetection: ComplexDomainOnsetDetection blockSize=512 stepSize=256')
+    fp.addFeature('loudness: Loudness blockSize=512 stepSize=256')
     data_flow = fp.getDataFlow()
     engine.load(data_flow)
     file_processor.processFile(engine, filename)
@@ -51,43 +52,64 @@ def convert(filename):
 
 if __name__ == "__main__":
     path = sys.argv[1]
-    files = [ f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) ]
+    files = os.walk(path)
     conn = sqlite3.connect("music.db")
     c = conn.cursor()
     c.execute("""
     CREATE TABLE IF NOT EXISTS songs (title varchar(255), artist varchar(255),
-    album varchar(255), mfcc varchar(255), size float, beat float);
+    album varchar(255), mfcc varchar(255), msize float, loudness varchar(255),
+    lsize float);
     """)
-    for filename in files:
-        if ".mp3" in filename:
-            metadata = convert(os.path.join(path, filename))
-            data = analyze_file(metadata["filename"])
-            data["mfcc"].flags.writeable = False
-            stdev = numpy.std(data["ComplexDomainOnsetDetection"])
-            total = 0
-            last = 0
-            count = 0
-            for val in range(len(data["ComplexDomainOnsetDetection"])):
-                total += data["ComplexDomainOnsetDetection"][val]
-                last = val
-                count += 1
-            if count:
-                beat = float(total)/float(count)
-                mfcc = data["mfcc"]
-                # precalculate what we need to combine cov
-                sigma, usigma = setup_cov(mfcc)
-                mean = numpy.mean(mfcc, 0) # calculate mean
-                n = mfcc.shape[0]
-                nmean = numpy.transpose([mean]*13)
-                cov =  numpy.cov(mfcc.T) # calculate covariance
-                mfccname = str(hash(data["mfcc"].data)).replace("-", "")
-                numpy.save("mfcc/%s-mean" % mfccname, mean)
-                numpy.save("mfcc/%s-cov" % mfccname, cov)
-                numpy.save("mfcc/%s-sigma" % mfccname, sigma)
-                numpy.save("mfcc/%s-usigma" % mfccname, usigma)
-                c.execute("""
-                INSERT INTO songs VALUES (?, ?, ?, ?, ?, ?);
-                """, (metadata["title"], metadata["artist"], metadata["album"],
-                    mfccname, mfcc.shape[0], beat))
-    conn.commit()
-    conn.close()
+    count = 0
+    for dirpath, dirnames, filenames in files:
+        for filename in filenames:
+            try:
+                if ".mp3" in filename:
+                    metadata = convert(os.path.join(dirpath, filename))
+                    previous = c.execute("""SELECT * FROM songs WHERE title=?""",
+                            ([metadata["title"]])).fetchall()
+                    if len(previous) == 0:
+                        data = analyze_file(metadata["filename"])
+                        data["mfcc"].flags.writeable = False
+                        data["loudness"].flags.writeable = False
+                        total = 0
+                        last = 0
+                        # precalculate what we need to combine mfcc cov
+                        mfcc = data["mfcc"]
+                        sigma, usigma = setup_cov(mfcc)
+                        mean = numpy.mean(mfcc, 0) # calculate mean
+                        nmean = numpy.transpose([mean]*13)
+                        cov =  numpy.cov(mfcc.T) # calculate covariance
+                        mfccname = str(hash(data["mfcc"].data)).replace("-", "")
+                        numpy.save("mfcc/%s-mean" % mfccname, mean)
+                        numpy.save("mfcc/%s-cov" % mfccname, cov)
+                        numpy.save("mfcc/%s-sigma" % mfccname, sigma)
+                        numpy.save("mfcc/%s-usigma" % mfccname, usigma)
+                        # precalculate what we need to combine loudness cov
+                        loudness= data["loudness"]
+                        sigma, usigma = setup_cov(loudness)
+                        mean = numpy.mean(loudness, 0) # calculate mean
+                        nmean = numpy.transpose([loudness]*loudness.shape[1])
+                        cov =  numpy.cov(loudness.T) # calculate covariance
+                        name = str(hash(loudness.data)).replace("-", "")
+                        numpy.save("loudness/%s-mean" % name, mean)
+                        numpy.save("loudness/%s-cov" % name, cov)
+                        numpy.save("loudness/%s-sigma" % name, sigma)
+                        numpy.save("loudness/%s-usigma" % name, usigma)
+                        c.execute("""
+                        INSERT INTO songs VALUES (?, ?, ?, ?, ?, ?, ?);
+                        """, (metadata["title"], metadata["artist"], metadata["album"],
+                            mfccname, mfcc.shape[0], name, loudness.shape[0]))
+                        count += 1
+                        print count/500.0
+                    else:
+                        print "You've alraedy analyzed %s" % metadata["title"]
+                    print "deleting %s" % metadata["filename"]
+                    os.remove(os.path.join(dirpath, metadata["filename"]))
+                if count % 50 == 0:
+                    conn.commit()
+                    conn.close()
+                    conn = sqlite3.connect("music.db")
+                    c = conn.cursor()
+            except Exception as e:
+                print e
